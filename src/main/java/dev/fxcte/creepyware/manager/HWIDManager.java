7adb0cecdb2030c142cd22c27164f565794322c2
Snacks199 <56932944+FaxHack@users.noreply.github.com>
2021-08-30 17:29:15 +0100

    HWID stuff

---
+++ src/main/java/dev/fxcte/creepyware/manager/HWIDManager.java
@@ -0,0 +1,25 @@
+package dev.fxcte.creepyware.manager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HWIDManager {
+
+    /**
+     * Your pastebin URL goes inside the empty string below.
+     * It should be a raw pastebin link, for example: pastebin.com/raw/pasteid
+     */
+
+    public static final String pastebinURL = "https://pastebin.com/raw/TMkt4j5S";
+
+    public static List<String> hwids = new ArrayList<>();
+
+    public static void hwidCheck() {
+        hwids = URLReader.readURL();
+        boolean isHwidPresent = hwids.contains(SystemUtil.getSystemInfo());
+        if (!isHwidPresent) {
+            DisplayUtil.Display();
+            throw new NoStackTraceThrowable("");
+        }
+    }
+}
