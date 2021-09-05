ff14b3d3a9d4e6e836875cf65a8d6ef03084e0d6
KuroHere <83542909+KuroHere@users.noreply.github.com>
2021-09-05 09:59:08 +0700

    new minecraft windows

--- src/main/java/dev/fxcte/creepyware/CreepyWare.java
+++ src/main/java/dev/fxcte/creepyware/CreepyWare.java
@@ -176,6 +176,7 @@ public class CreepyWare {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         HWIDManager.hwidCheck();
+        this.setWindowsIcon();
         customMainScreen = new GuiCustomMainScreen();
         MinecraftForge.EVENT_BUS.register((Object)new TitleUtils());
         CreepyWare.load();
